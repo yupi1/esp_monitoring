06e68b224255a2e678fce943186f3296339be2c2
yupi1 <toxahitech@gmail.com>
2026-05-09 11:18:30 +0300

    Uart receive from STM32

--- main/esp_monitor.c
+++ main/esp_monitor.c
@@ -1,23 +1,34 @@
 #include <stdio.h>
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
+#include <driver/gpio.h>
 #include "driver/uart.h"
 
 void app_main(void)
 {
-    const uart_port_t uart_num = UART_NUM_2;
+    uint8_t data[64];
+    const uart_port_t uart_port = UART_NUM_2; // Канал(порт) UART
+    const int uart_buffer_size = 128 * 2;
+    // QueueHandle_t uart_queue;  // Це FreeRTOS queue для UART events.(4,5 параметри метода install)
+
+    uart_driver_install(uart_port, uart_buffer_size, uart_buffer_size, 0, NULL, 0);
+
     uart_config_t u_config = {
         .baud_rate = 115200,
         .data_bits = UART_DATA_8_BITS,
         .parity = UART_PARITY_DISABLE,
         .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
     };
 
-    uart_param_config(uart_num, &u_config);
-    uart_set_pin(uart_num, NULL, GPIO_NUM_4, -1, -1);
+    uart_param_config(uart_port, &u_config);
+    uart_set_pin(uart_port, UART_PIN_NO_CHANGE, GPIO_NUM_4, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
 
     while(1) {
-        printf("Hello1\n");
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        int len = uart_read_bytes(uart_port, data, sizeof(data), pdMS_TO_TICKS(1000));
+        if(len > 0) {
+            printf("RX: %.*s\n", len, data);
+        }
     }
 }
